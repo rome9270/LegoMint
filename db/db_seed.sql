c7a8bda2837d68181b08838f160137b8600d889b
rome9270 <roland.metzger@fosbosweiden.de>
2025-10-23 21:04:50 +0200

    re

--- db/db_seed.sql
+++ db/db_seed.sql
@@ -1,9 +1,11 @@
---Übersicht der Aufgaben 
+-- Übersicht der Aufgaben
+
 DELETE FROM tasks;
 
-INSERT INTO tasks(title,description,html_file) VALUES
-('EV3 · Two Beeps & Name','Pybricks Step 1','03_01LegoEV3.html'),
-('EV3 · square','Pybricks Step 1','03_02LegoEV3.html'),
-('EV3 · Ultraschall – Stop vor der Wand','Stoppe 150 mm vor der Wand','03_06LegoEV3.html'),
-('Python · Grundlagen','Einstieg','02_python_overview.html'),
-('EV2','nichts','0403.html');
+INSERT INTO tasks (title, description, html_file) VALUES
+  ('EV3 - beep & name', 'Pybricks Step 1', '03_01LegoEV3.html'),
+  ('EV3 - square', 'Pybricks Step 2', '03_02LegoEV3.html'),
+  ('EV3 - circle', 'Pybricks Step 3', '03_03LegoEV3.html'),
+  ('EV3 - eight', 'Pybricks Step 4', '03_04LegoEV3.html'),
+  ('EV3 - ultrasonic stop in front of a wall', 'Stoppe 150 mm vor der Wand', '03_05LegoEV3.html'),
+  ('EV3 - find the door', 'Ausgang finden mit Gyro', '03_06LegoEV3.html');
