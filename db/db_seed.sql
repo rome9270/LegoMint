092e8f3fabd533d93b0bb38508bbfe317584308a
rome9270 <roland.metzger@fosbosweiden.de>
2025-10-25 16:55:28 +0200

    pius

--- db/db_seed.sql
+++ db/db_seed.sql
@@ -8,4 +8,6 @@ INSERT INTO tasks (title, description, html_file) VALUES
 ('EV3 - circle', 'Pybricks Step 3', '03_03LegoEV3_I.html'),
 ('EV3 - eight', 'Pybricks Step 4', '03_04LegoEV3_I.html'),
 ('EV3 - ultrasonic stop in front of a wall', 'Stoppe 150 mm vor der Wand', '03_05LegoEV3_I.html'),
-('EV3 - find the door', 'Ausgang finden mit Gyro', '03_06LegoEV3_I.html');
+('EV3 - find the door', 'Ausgang finden mit Gyro', '03_06LegoEV3_I.html'),
+('EV3 - eight', 'Pybricks Step 4', '03_07LegoEV3_I.html'),
+('EV3 - eight', 'Pybricks Step 4', '03_08LegoEV3_I.html');
