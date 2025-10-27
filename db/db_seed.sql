02f7dd43a5a5cce2ef6a2099c0982910e4f50d1f
rome9270 <roland.metzger@fosbosweiden.de>
2025-10-27 06:57:53 +0100

    fix

--- db/db_seed.sql
+++ db/db_seed.sql
@@ -8,10 +8,10 @@ INSERT INTO tasks (title, html_file) VALUES
 ('EV3 - drive a circle', '03_03LegoEV3_I.html'),
 ('EV3 - drive a figure 8', '03_04LegoEV3_I.html'),
 ('EV3 - stop in front of a wall', '03_05LegoEV3_I.html'),
-('EV3 - find the door', '03_06LegoEV3_I.html'),
-('EV3 - eight', '03_07LegoEV3_I.html'),
-('EV3 - eight', '03_08LegoEV3_I.html'),
-('EV3 - eight', '03_09LegoEV3_I.html'),
-('EV3 - line follower', '03_10LegoEV3_I.html'),
-('EV3 - pick up line follower', '04_01LegoEV3_I.html'),
-('EV3 - eight','04_02LegoEV3_I.html');
+('EV3 - find the door(touch)', '03_06LegoEV3_I.html'),
+('EV3 - find the door(touch&gyro)', '03_07LegoEV3_I.html'),
+('EV3 - drive a square with gyro', '03_08LegoEV3_I.html'),
+('EV3 - line follower', '03_09LegoEV3_I.html'),
+('EV3 - line follower&touch', '03_10LegoEV3_I.html'),
+('EV3 - warehouse mission', '04_01LegoEV3_I.html'),
+('EV3 - warehouse mission&person safety','04_02LegoEV3_I.html');
